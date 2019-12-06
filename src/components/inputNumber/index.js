17e2657861811e6ed2f02d48ffa87d0f11718324
Dengbk <963021525@qq.com>
2019-12-06 15:24:50 +0800

    优化input代码

--- src/components/inputNumber/index.js
+++ src/components/inputNumber/index.js
@@ -1,18 +1,14 @@
 import React, { useState, useEffect, useMemo } from 'react';
 
 export default function InputNumber(props) {
-  // 定义组件内部value变量
-  const [myValue, setMyValue] = useState('');
+  // 定义组件内部value变量， 如果props传进了defaultValue，则value初始值设为defaultValue
+  const [myValue, setMyValue] = useState(props.defaultValue || '');
   // 定义 controlled 变量， 标记 是否受控 ，通过props中是否存在value来判断
   const controlled = useMemo(() => "value" in props, [props]);
   // 定义input框绑定的值 showValue， 通过 controlled来判断showValue的取值；
   // 受控时取 props传进来的value属性值； 非受控时 取组件内部value 
   const showValue = useMemo(() => controlled ? props.value : myValue, [controlled, props.value, myValue]);
   const { onChange } = props;
-  // 监听controlled 变量，如果非受控时 把props传进来的defaultValue赋值给 input内部value的值
-  useEffect(() => {
-    if (!controlled) setMyValue(props.defaultValue)
-  }, [controlled, props.defaultValue])
   return (
     <input
       type="text"
